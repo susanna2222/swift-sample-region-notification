5efd5a705af692133f329e67798d2f71eb57fca5
Liling Chen <liling.chen@ccbji.co.jp>
2019-08-20 13:14:29 +0900

    Initial Commit

---
+++ swift-sample-region-notification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-sample-region-notification
+//
+//  Created by Liling Chen on 2019/08/20.
+//  Copyright © 2019 CCBJI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
